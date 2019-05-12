e760ecdd1f9e863c01d27fa86f356b1f5ddc81c0
bogomilmihaylov <bogomilmihaylov@live.com>
2019-05-12 11:26:27 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,16 +2,16 @@ def valid_move?(board,index)
  index.between(0,8)
 
  if (!position_taken? && board[index] < 9)
-   return = TRUE
+   return == TRUE
    
   else if board[index] > 8
-    return = FALSE
+    return == FALSE
    
    else if (position_taken?) && board[index] < 9)
-     return = FALSE
+     return == FALSE
     
    else if (position_taken?) && board[index] > 8
-     return = FALSE
+     return == FALSE
 end
 end
 
